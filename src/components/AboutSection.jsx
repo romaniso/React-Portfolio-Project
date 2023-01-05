6e3672b38cad651e5560f65463c2d8af4935ef54
Roman Isopenko <isopenkoroman@gmail.com>
2023-01-05 11:09:38 +0100

    Fix padding of navbar items, restyle about section flex

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -8,24 +8,26 @@ import AboutImg from "../assets/images/transparentMe.png";
 export default function AboutSection() {
   return (
     <div>
-      <div className="container aboutSection__container">
-        <div className="aboutSection__left">
-          <TitleSection
-            className="title"
-            subheading="Let's get to know"
-            heading="About me"
-          />
-          <PText>
-            I am a beginner front end developer who loves creating and learning.
-            Stick around here and I'll show you what I can.
-          </PText>
-          <div className="aboutSection__buttons">
-            <Button btnLink="/projects" btnText="Works" />
-            <Button btnLink="/about" btnText="Read More" outline={true} />
+      <div className="container">
+        <div className="aboutSection__container">
+          <div className="aboutSection__left">
+            <TitleSection
+              className="title"
+              subheading="Let's get to know"
+              heading="About me"
+            />
+            <PText>
+              I am a beginner front end developer who loves creating and
+              learning. Stick around here and I'll show you what I can.
+            </PText>
+            <div className="aboutSection__buttons">
+              <Button btnLink="/projects" btnText="Works" />
+              <Button btnLink="/about" btnText="Read More" outline={true} />
+            </div>
           </div>
-        </div>
-        <div className="aboutSection__right">
-          <img className="aboutSection__img" src={AboutImg} alt="" />
+          <div className="aboutSection__right">
+            <img className="aboutSection__img" src={AboutImg} alt="" />
+          </div>
         </div>
       </div>
     </div>
