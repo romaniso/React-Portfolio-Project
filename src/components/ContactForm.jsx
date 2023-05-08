d60e06cc969fe754e1565bd6c819dc654ce60c17
Roman Isopenko <isopenkoroman@gmail.com>
2023-05-08 22:05:00 +0200

    Update emailjs credentials, fix  submit issue

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 
 const Result = () => {
@@ -10,6 +10,8 @@ const Result = () => {
 };
 
 const ContactForm = () => {
+  const form = useRef();
+
   const [formData, setFormData] = useState({
     user_email: "",
     user_name: "",
@@ -25,10 +27,10 @@ const ContactForm = () => {
 
     emailjs
       .sendForm(
-        process.env.REACT_APP_EMAILJS_SERVICE_ID,
-        process.env.YOUR_TEMPLATE_ID,
-        formData,
-        process.env.YOUR_PUBLIC_KEY
+        "service_r6r1q4l",
+        "temp_my_react_portfo",
+        form.current,
+        "gzuG-8n8L9RcvpbJh"
       )
       .then(
         (result) => {
@@ -54,7 +56,7 @@ const ContactForm = () => {
 
   return (
     <div className="form__wrapper">
-      <form className="form">
+      <form className="form" ref={form} onSubmit={sendEmail}>
         <div className="form-group">
           <label htmlFor="user_name">Name</label>
           <input
@@ -95,7 +97,6 @@ const ContactForm = () => {
           className="form__button button"
           type="submit"
           value="Send me a message"
-          onClick={(e) => sendEmail(e)}
         />
       </form>
       <div className="row">{result ? <Result /> : null}</div>
@@ -104,103 +105,3 @@ const ContactForm = () => {
 };
 
 export default ContactForm;
-
-// Old version - it doesn't work, maybe because of useState hooks
-
-// import React, { useState, useRef } from "react";
-// import emailjs from "@emailjs/browser";
-
-// const Result = () => {
-//   return (
-//     <p className="form__result">
-//       Your message has been sent. I will contact you soon.
-//     </p>
-//   );
-// };
-
-// export default function ContactForm() {
-//   const [name, setName] = useState("");
-//   const [email, setEmail] = useState("");
-//   const [message, setMessage] = useState("");
-
-//   const form = useRef();
-
-//   const [result, showResult] = useState(false);
-//   const sendEmail = (e) => {
-//     e.preventDefault();
-//     emailjs
-//       .sendForm(
-//         "service_r6r1q4l",
-//         "template_ddsxtrm",
-//         form.current,
-//         "gzuG-8n8L9RcvpbJh"
-//       )
-//       .then(
-//         (result) => {
-//           console.log(result.text);
-//         },
-//         (error) => {
-//           console.log(error.text);
-//         }
-//       );
-//     e.target.reset();
-//     showResult(true);
-//   };
-
-//   setTimeout(() => {
-//     showResult(false);
-//   }, 10000);
-
-//   return (
-//     <div className="form__wrapper">
-//       <form className="form" ref={form} onSubmit={sendEmail}>
-//         <div className="form-group">
-//           <label htmlFor="name">
-//             Your name
-//             <input
-//               className="card"
-//               type="text"
-//               id="name"
-//               name="user_name"
-//               value={name}
-//               onChange={(e) => setName(e.target.value)}
-//               required
-//             />
-//           </label>
-//         </div>
-//         <div className="form-group">
-//           <label htmlFor="email">
-//             Your email
-//             <input
-//               className="card"
-//               type="email"
-//               id="email"
-//               name="user_email"
-//               value={email}
-//               onChange={(e) => setEmail(e.target.value)}
-//               required
-//             />
-//           </label>
-//         </div>
-//         <div className="form-group">
-//           <label htmlFor="message">
-//             Your message
-//             <textarea
-//               className="card"
-//               type="text"
-//               id="message"
-//               name="message"
-//               value={message}
-//               onChange={(e) => setMessage(e.target.value)}
-//               required
-//             />
-//           </label>
-//         </div>
-//         <button className="form__button button" type="submit">
-//           Send me a message
-//         </button>
-//         <div className="row">{result ? <Result /> : null}</div>
-//       </form>
-//     </div>
-//   );
-// }
